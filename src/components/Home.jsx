972a1ae2952b03775eef5a629756cb1366fb84c1
Andrew <andrewboug@hotmail.com>
2024-01-24 17:30:46 -0600

    added pizza slice icon to webpage bar, changed home component to 30 second video of fire, added svg file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,61 +1,12 @@
 export default function Home() {
   return (
     <div id="home">
-      <h1>The home page</h1>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
-      <li>this is garbage </li>
+      <iframe
+        src="https://drive.google.com/file/d/1lwQdQP3BHQNkfDH1T0yl1hWDJXW0sxbx/preview"
+        width="640"
+        height="480"
+        allow="autoplay"
+      ></iframe>
     </div>
   );
 }
